c536bfd1da4ce4a6ecf75ac9b646aee88b444cb3
akashverman143 <akashverman@gmail.com>
2018-12-16 18:37:22 +0530

    improve logic

--- script1.js
+++ script1.js
@@ -7,110 +7,87 @@ var box = document.getElementById("objectDetails");
 
 var myObj = {};
 
+function toggleBtnEnableDisable(btnArray, boolValue) {
+  btnArray.forEach(function(btn) {
+    btn.disabled = boolValue;
+  })
+}
+
 
 //<-----Change event for user name-------->
 
 form.addEventListener('change', function (e) {
-	e.preventDefault();
-	if(userName.value.length <= 10) {
-      if( userName.value.trim() === "" ) {
-        alert( "Please provide your name!" );
-        userName.value = " ";
-        userAge.disabled = true;
-        memberShip.disabled = true;
-        submit.disabled = true;
-        userAge.className = "changeColor";
-        memberShip.className = "changeColor";
-        submit.className = "changeColor";
-        console.log("a");
-        return false;
-    } else {
-        alert("atleast 10 characters");
-        userAge.disabled = true;
-        memberShip.disabled = true;
-        submit.disabled = true;
-        userName.value = "";
-        userAge.className = "changeColor";
-        memberShip.className = "changeColor";
-        submit.className = "changeColor";
-        console.log("b");
-        return false;     
-        //console.log("j");
-    }     
-	} else {
-        userAge.disabled = false;
-        memberShip.disabled = false;
-        submit.disabled = false;
+  e.preventDefault();
+	if(userName.value.length <= 10 || userName.value.trim() === '' ) {
+    // userAge.disabled = true;
+    // memberShip.disabled = true;
+    // submit.disabled = true;
+    toggleBtnEnableDisable([userAge, memberShip, submit ], true);
+    userAge.className = "changeColor";
+    memberShip.className = "changeColor";
+    submit.className = "changeColor";
+    console.log("a");
+    userName.value.trim() === '' ? alert( "Please provide your name!" ): alert( "userName  should not be less than 10 char" )
+    userName.value = " ";
+  } else {
+        // userAge.disabled = false;
+        // memberShip.disabled = false;
+        // submit.disabled = false;
+        toggleBtnEnableDisable([userAge, memberShip, submit ], false);
         userAge.className = "";
         memberShip.className = "";
         submit.className = "";
         console.log("c");
-        return;
   }
-    
-});
-
-//<----------Focusout event for user age---------->
-
-userAge.addEventListener('focusout', function (e) {
-	e.preventDefault();
-	if (isNaN(userAge.value) || userAge.value < 1 ) {
-		  if(userAge.value.trim()  === "") {
-        memberShip.disabled = true;
-        submit.disabled = true;
-        alert("The age should not be blank.");
-        userAge.value = "";
+  console.log(e.target.value);
+console.log(e.target.id);
+  if (e.target.id === 'userAge') {
+    if (isNaN(userAge.value) || userAge.value < 1 ||userAge.value.trim()  === "") {
+        // memberShip.disabled = true;
+        // submit.disabled = true;
+        toggleBtnEnableDisable([memberShip, submit ], true);
         memberShip.className = "changeColor";
         submit.className = "changeColor";
         console.log("d");
-        return;
-    } else {
-        alert("The age must be a number.");
+        userAge.value.trim() === '' ? alert( "The age should not be blank." ): alert( "The age must be a number." );
         userAge.value = "";
-        submit.disabled = true;
-        memberShip.disabled = true;
-        memberShip.className = "changeColor";
-        submit.className = "changeColor";
-        console.log("e");
-    } 
-	} else {
-        submit.disabled = false;
-        memberShip.disabled = false;
-        memberShip.className = "";
-        submit.className = "";
-        console.log("f");
+    } else {
+      // submit.disabled = false;
+      // memberShip.disabled = false;
+      toggleBtnEnableDisable([memberShip, submit ], false);
+      memberShip.className = "";
+      submit.className = "";
+      console.log("f");
+    }
   }
+  
 });
 
 //<-------Click event for button and geting object value--------->
 
 submit.addEventListener("click", function(e) {
-	e.preventDefault();
+  e.preventDefault();
 	myObj.name = userName.value;
 	myObj.age = userAge.value;
 	myObj.membership = memberShip.value;
 	console.log(myObj);
 	var para = document.createElement("p");
-	para.textContent = "My Object = {  name : " + myObj.name;
+  para.textContent = "My Object = { \r\n name : " + myObj.name + "\r\n" +" age : " + myObj.age + "\r\n" + " membership : " + myObj.membership + " }";
+  para.className = "newLine";
 	box.appendChild(para);
-	var para1 = document.createElement("p");
-	para1.textContent = "age : " + myObj.age;
-	box.appendChild(para1);
-	var para2 = document.createElement("p");
-	para2.textContent = "membership : " + myObj.membership + " }";
-	box.appendChild(para2);
 	box.className = "myObjectDetails";
 	userName.value =" ";
 	userAge.value = " ";
 	memberShip.value = " ";
-	console.log(box);
+  console.log(box);
+  console.log(e.target.value);
 });
 
 //<------change event for making object empty-------->
 
 userName.addEventListener("click", function (e){
 	e.preventDefault();
-		box.textContent = " ";
-		box.className = "changeMyObjectDetails";
+	box.textContent = " ";
+	box.className = "changeMyObjectDetails";
 })
 
